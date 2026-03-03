524d421591b624b824fb2d711039a654d2c4d70a
Emanuel Perna <cuev4x@hotmail.com>
2026-03-03 17:40:12 -0300

    Init.Python C1/2

---
+++ Clases/Clase 1 y 2/clase1.py
@@ -0,0 +1,8 @@
+#Ejercicio 1: Escribir un programa que imprima un mensaje en la pantalla
+
+print ("hola mundo!😍🐱‍🏍")
+print ("¡hola mundo! mi nombre es Emanuel y este es mi primier programa!!! 🖥️ 🚀")
+print ("Este es mi super saludo 👋 con Python 🐍")
+
+saludo = "hola mundo"
+print (saludo)
