bc5b23bd67a3e01b8e2d7b089c718fa7e1180363
Emanuel Perna <cuev4x@hotmail.com>
2026-03-03 17:41:20 -0300

    Init.Python C/4

---
+++ Clases/Clase 4/clase4_variables_input_ejer.py
@@ -0,0 +1,123 @@
+#Input:Ejemplo
+
+Nombre = input("ingrese su nombre")
+Apellido = input("Ingrese su apellido")
+
+print(f"El nombre y apellido ingresado es {Nombre}{Apellido}")
+
+
+
+
+#Variable:Ejemplo 1
+
+x = "hola"
+variable = x
+print(variable) 
+
+
+print(10+10)
+
+numero1=100
+print(numero1 +100)
+
+
+numero1=100
+numero2=100
+print(numero1+numero2)
+
+
+
+
+#Variable Ejemplo 2:
+
+numero1 = 10
+numero2 = 20
+numero3 = "Hola"
+numero4 = "Mundo"
+numero5 = "Cruel"
+
+resultado = numero1 * numero2
+print(resultado)
+
+resultado = numero3 + numero4 * 5
+print(resultado)
+
+
+
+
+#Ejercicios : 1
+
+N1 = "hola mundo "
+N2 = " hermosa mañana verdad?" 
+N1 = N1 + ( N2 *2)
+print (N1)
+
+
+
+
+#Ejercicios : 2
+
+'''
+Solicitar al usuario que ingrese dos numeros y luego mostrar la suma de los dos numeros ingresados
+'''
+
+numero1 = input("ingrese numero 1:")
+numero2 = input("ingrese numero 2:")
+print (f"La suma de los dos numeros ingresados es {numero1+numero2}")
+
+
+
+
+#Ejercicios : 3
+
+'''
+Escribir un programa que solicite al usuario que ingrese su nombre y luego mostrarlo en un mensaje
+'''
+
+nombre = input("ingrese su nombre")
+print(f"Hola le saludo {nombre}")
+
+
+
+
+#Ejercicios : 4
+
+'''
+Escribir un programa que solicite al usuario que ingrese su apellido y nombre,email y telefono y luego mostrar estos datos en un mensaje
+'''
+
+apellido_nombre = input ("ingrese su apellido y nombre")
+email = input ("ingrese su email")
+telefono = input ("ingrese su numero telefonico")
+
+print (f"Hola, mi nombre es {apellido_nombre} , te paso mi contacto telefonico {telefono} y mi email {email}")
+
+
+
+
+#Ejercicios : 5
+
+'''
+Solicitar al usuario que ingrese como estuvo su dia, su edad y su ciudad de residencia, luego mostrar estos datos en un mensaje
+'''
+
+dia = input ("ingrese como estuvo su dia:")
+print (f"Mi dia estuvo de {dia}") 
+
+edad = input ("ingrese su edad:")
+residencia = input ("ingrese su ciudad de residencia")
+
+print (f" el usuario tiene {edad} años ,y su lugar de residencia es {residencia}")
+
+
+
+#Ejercicios : 6
+
+'''
+Solicitar al usuario que ingrese su comida favorita y su bebida favorita, luego mostrar un mensaje que contenga ambas respuestas
+'''
+
+comida = input ("ingrese su comida favorita")
+bebida = input ("ingrese su bebida favorita")
+
+print (f"la comida y bebida favorita del usuario es {comida} y {bebida}")
