25a4a6dec01b4e4232c468917b18d80cc62e8a6e
Emanuel Perna <cuev4x@hotmail.com>
2026-03-03 17:40:50 -0300

    Init.Python C/3

---
+++ Clases/Clase 3/clase3_ejercicios.py
@@ -0,0 +1,36 @@
+#Ejercicio Numero 1: Escribir 3 variables para que almacene Apellido, Nombre , Email y Telefono
+apellido_nombre = ""
+email = ""
+telefono =  ""
+
+print (apellido_nombre)
+print (email)
+print (telefono)
+
+#Ejercicio Numero 2: Escribir un programa que sume dos numeros
+
+x = 16
+y = 2
+z = (x+y)
+
+print(z)
+
+
+#Ejercicio Numero 3:
+'''
+Escribir un programa donde una variable llamada contador cambie 
+a lo largo de la ejecucion del programa, su primer valor sea 0, luego cambie a 5 
+y posteriormente a 25 y para finalizar el programa volver a ser 5
+
+'''
+contador = 0
+contador = 5
+contador = 25
+contador = 5
+
+print(f" {contador}")
+
+#Ejercicio Numero 4:
+
+mensaje = "Dia"
+print(f"Buen {mensaje}")
